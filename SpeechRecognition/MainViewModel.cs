ccbf578150a7b8c871cc4c0fc288ce3346dc8a55
Leo Alps <leonardo@cumulusdental.com>
2018-07-31 17:24:28 -0400

    First commit

---
+++ SpeechRecognition/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace SpeechRecognition
+{
+    public class MainViewModel : ViewModelBase, IDisposable
+    {
+        private SpeechRecognitionEngine _recognizerEngine;
+
+        public ICommand ActivateRecognitionCommand { get; private set; }
+
+        private string _recognizedText;
+
+        public string RecognizedText
+        {
+            get
+            {
+                return _recognizedText;
+            }
+
+            set
+            {
+                Set(ref _recognizedText, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            ActivateRecognitionCommand = new RelayCommand(ActivateRecognition);
+
+            InitializeRecognitionEngine();
+        }
+
+        private void InitializeRecognitionEngine()
+        {
+            _recognizerEngine = new SpeechRecognitionEngine();
+            _recognizerEngine.SetInputToDefaultAudioDevice();
+        }
+
+        private void ActivateRecognition()
+        {
+            RecognizedText = string.Empty;
+            _recognizerEngine.RecognizeAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_recognizerEngine != null)
+            {
+                _recognizerEngine.Dispose();
+            }
+        }
+    }
+}
---
+++ SpeechRecognition/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace SpeechRecognition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        private void MainWindow_OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainViewModel viewModel = DataContext as MainViewModel;
+
+            if (viewModel != null)
+            {
+                viewModel.Dispose();
+            }
+        }
+    }
+}
